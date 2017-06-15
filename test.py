b697fa2b90837836e07bf2f308a2c45645be895d
kbogas <bogas.ko@gmail.com>
2017-06-15 15:34:11 +0300

    Added README.md

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python !/usr/bin/env python
 # -*- coding: utf-8 -*
 
+# !!!!!!!!!  FIRST CONFIGURE SETTINGS.YAML TO MATCH YOUR NEEDS !!!!!!!!!!
+# Simple script to run the Pipeline Wrapper,
+
+
 import logging
 from tasks import taskCoordinator
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
-a = taskCoordinator()
-a.print_pipeline()
-a.run()
+
+
+TaskManager = taskCoordinator()
+TaskManager.print_pipeline()
+TaskManager.run()
+exit(1)
